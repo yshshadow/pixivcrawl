b70bd5420f0f406923031283976fb36b67d7eece
yshshadow <ysh3517531@qq.com>
2016-03-03 17:21:16 +0800

    change item

--- pixiv/pixiv/items.py
+++ pixiv/pixiv/items.py
@@ -7,23 +7,6 @@
 
 import scrapy
 
-
-class PixivCrawlItem(scrapy.Item):
-    title = scrapy.Field()
-    date = scrapy.Field()
-    img_urls = scrapy.Field()
-    user_id = scrapy.Field()
-    user_name = scrapy.Field()
-    rank = scrapy.Field()
-    yes_rank = scrapy.Field()
-    total_score = scrapy.Field()
-    views = scrapy.Field()
-    is_sexual = scrapy.Field()
-    illust_id = scrapy.Field()
-    tags = scrapy.Field()
-    url = scrapy.Field()
-    image_paths = scrapy.Field()
-
 class PixivPicItem(scrapy.Item):
     title = scrapy.Field()
     date = scrapy.Field()
