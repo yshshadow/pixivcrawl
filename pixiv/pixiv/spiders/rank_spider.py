ef91cef1bf68b3c3d5e7ac2e550a578005e32b78
yshshadow <ysh3517531@qq.com>
2016-03-12 16:16:08 +0800

    完成热榜抓取功能

--- pixiv/pixiv/spiders/rank_spider.py
+++ pixiv/pixiv/spiders/rank_spider.py
@@ -35,12 +35,31 @@ class RankSpiderSpider(scrapy.Spider):
             item['title'] = pic.xpath('@data-title').extract()
             item['user_id'] = pic.xpath('@data-user-name').extract()
             item['user_name'] = pic.xpath('a[@class="user-container ui-profile-popup"]/@data-user_id').extract()
-            item['url'] = pic.xpath('div[@class="ranking-image-item"]/a[@class="work  _work "]/@href').extract()
-            yield item
-            
+            detail_urls = pic.xpath('div[@class="ranking-image-item"]/a[@class="work  _work "]/@href').extract()
+            detail_url = ''
+            for url in detail_urls:
+                detail_url = url
+            yield scrapy.Request(
+                    self.generate_detail_url(detail_url),
+                    callback=self.parse_detail,
+                    meta={'item':item},
+                    headers={
+                        'referer':response.url,
+                        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) '
+                        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
+                        }
+                    )
+
+    def parse_detail(self,response):
+        item = response.meta['item']
+        item['url'] = response.url
+        img_url = response.xpath('//img[@class="original-image"]/@data-src').extract()
+        if(len(img_url)>0):
+            item['img_urls'] = img_url
+        yield item
 
     def generate_detail_url(self,detail_url):
-        pass
+        return 'http://www.pixiv.net/{0}'.format(detail_url)
 
     def generate_rank_url(self,page=1,date=datetime.date.today(),mode='daily'):
         rank_url = 'http://www.pixiv.net/ranking.php?mode={mode}&date={date}&p={page}'
